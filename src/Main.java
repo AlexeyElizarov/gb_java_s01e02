b4d6113d7932c64d0b9f337d5559d94e1363402a
AlexeyElizarov <34809281+AlexeyElizarov@users.noreply.github.com>
2020-10-25 08:40:31 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+        boolean toCheck = false;
+        
         // Задание 1
         print("Задание 1");
         doTask1();
